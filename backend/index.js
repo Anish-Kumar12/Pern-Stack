0da187f5281b58b6d58c51de3d7090d51f1b356d
Anish Kumar <anishkumar344567@gmail.com>
2025-02-19 19:37:55 +0530

    Added helmet morgan cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
 import express from "express"
-const app = express()
+import helmet from "helmet"
+import morgan from "morgan";
+import cors from 'cors'
+import dotenv from 'dotenv'
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3000;
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("dev"))
+app.use(cors())
+
+
+app.listen(PORT, () => {
+    console.log("Server is running on port",PORT)
     });
